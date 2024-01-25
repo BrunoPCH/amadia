0fdd7ec16d4e5f4ff4340ea84abda8a646514726
BrunoPCH <bruno.perez.ch@gmail.com>
2024-01-25 15:07:41 -0600

    optimized images

--- src/components/ImgExperticeAreas.jsx
+++ src/components/ImgExperticeAreas.jsx
@@ -1,11 +1,14 @@
 import "../styles/ImgExperticeAreas.css";
-import imgExperticeFamily from "../assets/img-expertice-family.png";
+import imgExperticeFamilyMd from "../assets/familia-600.webp";
+import imgExperticeFamilySm from "../assets/familia-300.webp";
+import imgExperticeFamilyLg from "../assets/familia-900.webp";
 const ImgExperticeAreas = () => {
   return (
     <>
       <img
         className="img-expertice-area"
-        src={imgExperticeFamily}
+        src={imgExperticeFamilyMd}
+        srcSet={`${imgExperticeFamilySm} 300w, ${imgExperticeFamilyMd} 600w, ${imgExperticeFamilyMd}   900w, ${imgExperticeFamilyLg} 1200w`}
         alt="Familia: Padre, madre e hija abrazados"
       />
     </>
